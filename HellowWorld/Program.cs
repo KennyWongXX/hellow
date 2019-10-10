4fa6616e89ad6cb428370eb0f45f6dc6211a681b
KennyWongXX <34906729+KennyWongXX@users.noreply.github.com>
2019-10-10 12:46:52 +0800

    Commit All Change

---
+++ HellowWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HellowWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string doubleForwardSlash = new string((char)47, 2); // /
+            string doubleBackSlash = new string((char)92, 2); // \
+
+            string hellow = "Hellow wourld";
+
+            Console.WriteLine(hellow);
+            Console.WriteLine();
+        }
+    }
+}
