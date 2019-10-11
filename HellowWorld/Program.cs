8ffbfa34c29bbae52db456acea10ab8b1e876c6c
KennyWongXX <34906729+KennyWongXX@users.noreply.github.com>
2019-10-11 12:37:37 +0800

    test

--- HellowWorld/Program.cs
+++ HellowWorld/Program.cs
@@ -9,7 +9,7 @@ namespace HellowWorld
             string doubleForwardSlash = new string((char)47, 2); // /
             string doubleBackSlash = new string((char)92, 2); // \
 
-            string hellow = "Hellow TEST";
+            string hellow = "Hellow x";
 
             Console.WriteLine(hellow);
             Console.WriteLine();
