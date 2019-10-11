8799dea74b02d10cbd1cd35d21c11fdb146989c9
KennyWongXX <34906729+KennyWongXX@users.noreply.github.com>
2019-10-11 12:28:20 +0800

    change from hellow branch1 to TEST

--- HellowWorld/Program.cs
+++ HellowWorld/Program.cs
@@ -9,7 +9,7 @@ namespace HellowWorld
             string doubleForwardSlash = new string((char)47, 2); // /
             string doubleBackSlash = new string((char)92, 2); // \
 
-            string hellow = "Hellow Bransh1";
+            string hellow = "Hellow TEST";
 
             Console.WriteLine(hellow);
             Console.WriteLine();
